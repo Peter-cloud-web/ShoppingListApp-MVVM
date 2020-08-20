c38c14f70d2a1e205c8c052db2a88d71dd2a9e5e
Peter-cloud-web <petergichia165@gmail.com>
2020-08-20 04:38:22 +0300

    Query the database

--- app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingDAO.java
+++ app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingDAO.java
@@ -1,4 +1,20 @@
 package com.seanpetersgichia.shoppinglistapp;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface ShoppingDAO {
+    @Insert
+    void insertItem(ShoppingModel... shoppingModels);
+
+    @Query("DELETE FROM shoppinglist_table")
+    void deleteItem(ShoppingModel...shoppingModel);
+
+    @Query("SELECT * from shoppinglist_table")
+    List<ShoppingModel> getAllItems();
 }
--- app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingModel.java
+++ app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingModel.java
@@ -1,6 +1,5 @@
 package com.seanpetersgichia.shoppinglistapp;
 
-import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
