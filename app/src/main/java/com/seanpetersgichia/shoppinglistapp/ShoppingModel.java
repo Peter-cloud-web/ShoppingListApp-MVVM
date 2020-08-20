9e0deb0c82fad10ad6500caf45bf733e92329339
Peter-cloud-web <petergichia165@gmail.com>
2020-08-20 04:25:27 +0300

    construct the shopping model to hold the associated entity and columns

---
+++ app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingModel.java
@@ -0,0 +1,18 @@
+package com.seanpetersgichia.shoppinglistapp;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "shoppinglist_table")
+public class ShoppingModel {
+
+    @PrimaryKey
+    public int id;
+
+    public String groceryName;
+
+    public int groceryQuantity;
+
+    public int groceryTotalPrice;
+}
