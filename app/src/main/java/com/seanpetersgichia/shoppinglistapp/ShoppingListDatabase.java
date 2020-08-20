ed3fed28efdcffd2aa2bb0b7b5b0940f35ca559f
Peter-cloud-web <petergichia165@gmail.com>
2020-08-20 05:30:19 +0300

    create singleton class for the database

--- app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingListDatabase.java
+++ app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingListDatabase.java
@@ -1,4 +1,33 @@
 package com.seanpetersgichia.shoppinglistapp;
 
-public class ShoppingListDatabase {
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = ShoppingListModel.class, version = 1, exportSchema = false)
+public abstract class ShoppingListDatabase extends RoomDatabase {
+
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    private static volatile ShoppingListDatabase INSTANCE;
+
+    public static synchronized ShoppingListDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (ShoppingListDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), ShoppingListDatabase.class, "shoppinglist_table")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public abstract ShoppingListDAO shoppingListDAO();
 }
