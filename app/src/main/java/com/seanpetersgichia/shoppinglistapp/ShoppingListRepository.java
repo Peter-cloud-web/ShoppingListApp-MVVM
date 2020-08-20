49a5d80bd009c75d5c79f50d2c58355f5ba3c77f
Peter-cloud-web <petergichia165@gmail.com>
2020-08-20 16:20:38 +0300

    using java 8 lambda expression to avoid boiler plate code when running the AsyncTask

--- app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingListRepository.java
+++ app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingListRepository.java
@@ -1,29 +1,34 @@
 package com.seanpetersgichia.shoppinglistapp;
 
 import android.app.Application;
+import android.content.Context;
+import android.os.AsyncTask;
 
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
 
 public class ShoppingListRepository {
-    private ShoppingListDAO mShoppingListDAO;
-    private LiveData<List<ShoppingListModel>> shoppingListTable;
+    private Context context;
 
-    ShoppingListRepository(Application application){
-        ShoppingListDatabase db = ShoppingListDatabase.getDatabase(application);
-        mShoppingListDAO = db.shoppingListDAO();
-        shoppingListTable = mShoppingListDAO.getAllItems();
+
+    public ShoppingListRepository(Context context) {
+        this.context = context.getApplicationContext();
     }
 
-    LiveData<List<ShoppingListModel>> getAllItemsSaved(){
-        return  shoppingListTable;
+    public void addNewItem(ShoppingListModel shoppingListModel){
+        AsyncTask.execute(() -> ShoppingListDatabase.getDatabase(context).shoppingListDAO().insertItem(shoppingListModel));
     }
 
-    void insert(final ShoppingListModel...shoppingListModels){
-        ShoppingListDatabase.databaseWriteExecutor.execute(() -> {
-            mShoppingListDAO.insertItem(shoppingListModels);
-        });
+    public LiveData<List<ShoppingListModel>> getItems(){
+        return ShoppingListDatabase.getDatabase(context).shoppingListDAO().getAllItems();
     }
 
+    public LiveData<Integer> getItemCount(){
+        return ShoppingListDatabase.getDatabase(context).shoppingListDAO().countAllItems();
+    }
+
+    public void deleteItem(ShoppingListModel shoppingListModel){
+        AsyncTask.execute(() -> ShoppingListDatabase.getDatabase(context).shoppingListDAO().deleteItem());
+    }
 }
