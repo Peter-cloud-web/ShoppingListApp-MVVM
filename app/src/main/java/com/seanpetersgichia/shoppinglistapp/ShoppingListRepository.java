6069deecc9f209e706bea670eb662c654d2ccaf2
Peter-cloud-web <petergichia165@gmail.com>
2020-08-20 16:03:23 +0300

    refactor repository class to use Asynctask

--- app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingListRepository.java
+++ app/src/main/java/com/seanpetersgichia/shoppinglistapp/ShoppingListRepository.java
@@ -1,4 +1,29 @@
 package com.seanpetersgichia.shoppinglistapp;
 
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class ShoppingListRepository {
+    private ShoppingListDAO mShoppingListDAO;
+    private LiveData<List<ShoppingListModel>> shoppingListTable;
+
+    ShoppingListRepository(Application application){
+        ShoppingListDatabase db = ShoppingListDatabase.getDatabase(application);
+        mShoppingListDAO = db.shoppingListDAO();
+        shoppingListTable = mShoppingListDAO.getAllItems();
+    }
+
+    LiveData<List<ShoppingListModel>> getAllItemsSaved(){
+        return  shoppingListTable;
+    }
+
+    void insert(final ShoppingListModel...shoppingListModels){
+        ShoppingListDatabase.databaseWriteExecutor.execute(() -> {
+            mShoppingListDAO.insertItem(shoppingListModels);
+        });
+    }
+
 }
