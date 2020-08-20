a7c6268ec65eca0a630fbcb5117404f441b88430
Peter-cloud-web <petergichia165@gmail.com>
2020-08-20 12:13:59 +0300

    Add a spinner to get the quantity of items

--- app/src/main/java/com/seanpetersgichia/shoppinglistapp/MainActivity.java
+++ app/src/main/java/com/seanpetersgichia/shoppinglistapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.seanpetersgichia.shoppinglistapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView countItems = findViewById(R.id.numberOfItems);
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        EditText inputItems = findViewById(R.id.inputItem);
+        Button saveItems = findViewById(R.id.addButton);
+
+        Spinner spinner = findViewById(R.id.inputQuantitySpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.item_count_array,android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
